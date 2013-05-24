70dae2839d8cbfd877a74bdec16bb379f5bd4140
Rémi Duraffort <ivoire@videolan.org>
2013-05-24 19:00:43 +0200

    Login page

--- RandoAmisSecours/urls.py
+++ RandoAmisSecours/urls.py
@@ -3,6 +3,12 @@
 
 from django.conf.urls import patterns, url
 
+# Main page
 urlpatterns = patterns('RandoAmisSecours.views.main',
-    url(r'^$', 'index', name='index')
+    url(r'^$', 'index', name='index'),
+)
+
+# Authentication
+urlpatterns += patterns('django.contrib.auth.views',
+    url(r'^accounts/login/$', 'login', {'template_name': 'RandoAmisSecours/registration/login.html'}, name='accounts.login'),
 )
