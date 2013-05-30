ee64138de1cb869e056462e6e0f3d5f99fa72bbf
Rémi Duraffort <ivoire@videolan.org>
2013-05-30 18:22:50 +0200

    First model draft
    Only basic profiles and outings are defined

--- RandoAmisSecours/models.py
+++ RandoAmisSecours/models.py
@@ -1,3 +1,42 @@
+# -*- coding: utf-8 -*-
+# vim: set ts=4
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+def random_hash():
+    """ Create a random string of size 15 """
+    return binascii.b2a_hex(os.urandom(15))
+
+
+class Profile(models.Model):
+    class Meta:
+        app_label = 'RandoAmisSecours'
+
+    user = models.ForeignKey(User)
+    phone_number = models.CharField(max_length=30)
+    hash_id = models.CharField(unique=True, max_length=30, default=random_hash)
+
+
+class Outing(models.Model):
+    class Meta:
+        app_label = 'RandoAmisSecours'
+
+    user = models.ForeignKey(User)
+
+    # Visible information
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    # Time frame: (begin, end, alert)
+    begining = models.DateTimeField()
+    ending = models.DateTimeField()
+    alert = models.DateTimeField()
+
+    # Position on the map
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __unicode__(self):
+        return u"%s: %s" % (self.user.get_full_name(), self.name)
