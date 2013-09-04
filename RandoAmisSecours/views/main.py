0f76da873dd23cf5304903ea5baa02767937b354
Rémi Duraffort <ivoire@videolan.org>
2013-09-04 20:37:58 +0200

    Add the headers for the AGPL

--- RandoAmisSecours/views/main.py
+++ RandoAmisSecours/views/main.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 # vim: set ts=4
 
+# Copyright 2013 Rémi Duraffort
+# This file is part of RandoAmisSecours.
+#
+# RandoAmisSecours is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RandoAmisSecours is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with RandoAmisSecours.  If not, see <http://www.gnu.org/licenses/>
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
