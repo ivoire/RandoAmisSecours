fad2d464006f90275e65ec0ad3d02073af4bf39f
Rémi Duraffort <ivoire@videolan.org>
2014-03-24 22:37:14 +0100

    Reporting: add a graph about last login date

--- RandoAmisSecours/views/reporting.py
+++ RandoAmisSecours/views/reporting.py
@@ -47,12 +47,18 @@ def users(request):
     now = datetime.utcnow().replace(tzinfo=utc)
     users_list = User.objects.all()
 
-    joining_dates = [0] * 365
+    joining_dates = [0] * 366
+    last_logins = [0] * 366
     for user in users_list:
         days_delta = (now - user.date_joined).days
         if days_delta <= 365:
             joining_dates[365 - days_delta] += 1
 
+        days_delta = (now - user.last_login).days
+        if days_delta <= 365:
+            last_logins[365 - days_delta] += 1
+
     return render_to_response('RandoAmisSecours/reporting/users.html',
-                              {'joining_dates': joining_dates},
+                              {'joining_dates': joining_dates,
+                               'last_logins': last_logins},
                               context_instance=RequestContext(request))
