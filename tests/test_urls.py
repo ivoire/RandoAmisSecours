69ee771e03a51de6abee8ed29abce09745f4ecf2
Rémi Duraffort <ivoire@videolan.org>
2016-03-18 21:52:17 +0100

    Fix tests

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,12 +1,9 @@
-from django.conf.urls import patterns, include, url
-
-from RandoAmisSecours import urls
-
-# Uncomment the next two lines to enable the admin:
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('RandoAmisSecours.urls')),
-)
+from RandoAmisSecours import urls as ras_urls
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include(ras_urls)),
+]
