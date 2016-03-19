58c3fc6b2a827fb7e9fe26bdd0c3861f26154cf7
Rémi Duraffort <ivoire@videolan.org>
2016-03-19 14:06:25 +0100

    Add default_app_config

--- RandoAmisSecours/__init__.py
+++ RandoAmisSecours/__init__.py
@@ -16,3 +16,5 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with RandoAmisSecours.  If not, see <http://www.gnu.org/licenses/>
+
+default_app_config = 'RandoAmisSecours.apps.RASConfig'
